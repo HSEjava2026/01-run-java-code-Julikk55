8fb1d9dcb2cd17bc9b3d6448b7f9ba607d3b76c4
Julik <juliazaika07@gmail.com>
2026-02-05 23:29:56 +0300

    Task 3

--- src/ru/hse/lab1/Hello.java
+++ src/ru/hse/lab1/Hello.java
@@ -1,6 +1,23 @@
 package ru.hse.lab1;
+/**
+ * Учебный класс для вывода "Hello, World!".
+ *
+ * @author Заика_Юлия
+ * @version 1.0
+ * @since 2026
+ */
 
 public class Hello {
+        /**
+     * Конструктор класса Hello.
+     */
+    public Hello() {
+    }
+        /**
+     * Точка входа учебной программы.
+     *
+     * @param args аргументы командной строки
+     */
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
--- src/ru/hse/lab1/second/Person.java
+++ src/ru/hse/lab1/second/Person.java
@@ -1,11 +1,33 @@
 package ru.hse.lab1.second;
 
+/**
+ * Класс Person хранит данные о человеке: имя и возраст.
+ *
+ * @author Заика_Юлия
+ * @version 1.0
+ * @since 2026
+ */
 public class Person {
-    public String name;
-    public int age;
+    private String name;
+    private int age;
 
+    /**
+     * Создаёт объект человека.
+     *
+     * @param name имя человека
+     * @param age  возраст человека
+     */
     public Person(String name, int age) {
-        this.age = age;
         this.name = name;
+        this.age = age;
+    }
+
+    /**
+     * Печатает информацию о человеке в консоль.
+     */
+    public void printInfo() {
+        System.out.println("Имя: " + name);
+        System.out.println("Возраст: " + age);
     }
 }
+
