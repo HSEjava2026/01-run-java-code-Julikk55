e7b6cb65508e1549da4ed074c28d976ad34b8043
Julik <juliazaika07@gmail.com>
2026-02-05 23:54:07 +0300

    Task 3 fixed

--- src/ru/hse/lab1/Main.java
+++ src/ru/hse/lab1/Main.java
@@ -2,28 +2,15 @@ package ru.hse.lab1;
 
 import ru.hse.lab1.second.Person;
 /**
- * Главный класс запуска программы.
  *
  * @author Заика_Юлия
  * @version 1.0
  * @since 2026
  */
-
 public class Main {
-        /**
-     * Конструктор класса Main.
-     */
-    public Main() {
-    }
 
-        /**
-     * Точка входа в программу.
-     *
-     * @param args аргументы командной строки
-     */
     public static void main(String[] args) {
         Person p = new Person("Иван", 21);
-        
-        System.out.println("Возраст: " + p.age + "\nИмя: " + p.name);
+        p.printInfo();
     }
 }
--- src/ru/hse/lab1/second/Person.java
+++ src/ru/hse/lab1/second/Person.java
@@ -1,7 +1,6 @@
 package ru.hse.lab1.second;
 
 /**
- * Класс Person хранит данные о человеке: имя и возраст.
  *
  * @author Заика_Юлия
  * @version 1.0
@@ -11,20 +10,13 @@ public class Person {
     private String name;
     private int age;
 
-    /**
-     * Создаёт объект человека.
-     *
-     * @param name имя человека
-     * @param age  возраст человека
-     */
+    
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    /**
-     * Печатает информацию о человеке в консоль.
-     */
+   
     public void printInfo() {
         System.out.println("Имя: " + name);
         System.out.println("Возраст: " + age);
